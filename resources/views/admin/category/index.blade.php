bdb559e10fb3b97705e8fa35eaafa61a6843d8ae
ikhfhm <ikhwanfahmihidayat@gmail.com>
2020-09-19 08:21:24 +0700

    add button action

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -2,8 +2,19 @@
 @section('content')
 <h1>INI ADALAH CATEGORY</h1>
 @foreach($category as $result)
-	<ul>
-		<li>{{$result->name}}</li>
-	</ul>
+	<table class="table table-striped table-hover table-sm table-bordered">
+		<thead>
+			<th>No</th>
+			<th>Nama Kategori</th>
+			<th>Aksi</th>
+		</thead>
+		<tbody>
+			<td></td>
+			<td>{{$result->name}}</td>
+			<td><a href="" class="btn btn-warning btn-sm">Edit</a>
+				<a href="" class="btn btn-danger btn-sm">Delete</a>
+			</td>
+		</tbody>
+	</table>
 @endforeach
 @endsection
