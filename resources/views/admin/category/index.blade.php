8e049ac44968bc15a5fc3b3ed827665b3eb77d2e
ikhfhm <ikhwanfahmihidayat@gmail.com>
2020-09-19 08:07:48 +0700

    print database to view index category

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -1,4 +1,9 @@
 @extends('template_backend.home')
 @section('content')
 <h1>INI ADALAH CATEGORY</h1>
+@foreach($category as $result)
+	<ul>
+		<li>{{$result->name}}</li>
+	</ul>
+@endforeach
 @endsection
