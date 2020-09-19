49e5f7a1e77ef4c29d18952fa7dc2714e1d00dde
ikhfhm <ikhwanfahmihidayat@gmail.com>
2020-09-19 08:25:32 +0700

    move foreach

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -1,7 +1,7 @@
 @extends('template_backend.home')
 @section('content')
 <h1>INI ADALAH CATEGORY</h1>
-@foreach($category as $result)
+
 	<table class="table table-striped table-hover table-sm table-bordered">
 		<thead>
 			<th>No</th>
@@ -9,12 +9,16 @@
 			<th>Aksi</th>
 		</thead>
 		<tbody>
-			<td></td>
-			<td>{{$result->name}}</td>
-			<td><a href="" class="btn btn-warning btn-sm">Edit</a>
-				<a href="" class="btn btn-danger btn-sm">Delete</a>
-			</td>
+			@foreach($category as $result)
+			<tr>
+				<td></td>
+				<td>{{$result->name}}</td>
+				<td><a href="" class="btn btn-warning btn-sm">Edit</a>
+					<a href="" class="btn btn-danger btn-sm">Delete</a>
+				</td>
+			</tr>
+			@endforeach
 		</tbody>
 	</table>
-@endforeach
+
 @endsection
