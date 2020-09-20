7fa5c539ad0c26cb66b5cb61630b495865c1c7a6
ikhfhm <ikhwanfahmihidayat@gmail.com>
2020-09-21 06:20:03 +0700

    add url for dashboard

--- resources/views/template_backend/sidebar.blade.php
+++ resources/views/template_backend/sidebar.blade.php
@@ -7,14 +7,7 @@
             <a href="index.html">St</a>
           </div>
           <ul class="sidebar-menu">
-            <li class="menu-header">Dashboard</li>
-            <li class="dropdown">
-              <a href="#" class="nav-link has-dropdown"><i class="fas fa-home"></i><span>Dashboard</span></a>
-              <ul class="dropdown-menu">
-                <li><a class="nav-link" href="index-0.html">General Dashboard</a></li>
-                <li><a class="nav-link" href="index.html">Ecommerce Dashboard</a></li>
-              </ul>
-            </li>
+            <li class=active><a class="nav-link" href="{{route('home')}}"><i class="fas fa-home"></i> <span>Dashboard</span></a></li>
             <li class="menu-header">Starter</li>
             <li class="dropdown">
               <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-book-open"></i> <span>Post</span></a>
@@ -34,6 +27,5 @@
                 <li><a class="nav-link" href="{{route('tag.index')}}">List Tag</a></li>
               </ul>
             </li>
-            <li class=active><a class="nav-link" href="blank.html"><i class="far fa-square"></i> <span>Blank Page</span></a></li>
         </aside>
       </div>
